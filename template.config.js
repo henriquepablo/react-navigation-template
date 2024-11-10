b7d3807f477958eabaf564865e0a9000618b1429
henriquepablo <pablo269704@gmail.com>
2024-11-10 01:27:25 -0300

    fix: template

--- template.config.js
+++ template.config.js
@@ -3,6 +3,13 @@ module.exports = {
   
     description: 'A template with React Navigation pre-configured',
   
-    template: './template',
+    templateDir: './',
+
+    dependencies: {
+        "@react-navigation/native": "^7.0.0",
+        "@react-navigation/native-stack": "^7.0.0",
+        "react-native-safe-area-context": "^4.14.0",
+        "react-native-screens": "^4.0.0"
+    }
 };
   
